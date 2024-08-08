843167391a4fce90114a22b5e08a1d75780e3054
Jesus Castro <castrojesus39@gmail.com>
2024-08-07 22:41:42 -0700

    added the connectionString for db and initiliazed it to create the table

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,11 +1,17 @@
-﻿using Spectre.Console;
+﻿using System.Configuration;
 
 namespace CodingTracker;
 
 internal static class Program
 {
+    private static readonly string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
     public static void Main(string[] args)
     {
+        DatabaseManager databaseManager = new();
+
+        databaseManager.CreateTable(connectionString);
+
         Menu.ShowMenu();
+
     }
 }
