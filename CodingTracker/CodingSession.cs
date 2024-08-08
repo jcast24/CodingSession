16ce7f63c4565524da2ad2e73443ca5cbdc2f060
Jesus Castro <castrojesus39@gmail.com>
2024-08-07 19:58:54 -0700

    added class properties for the entire project

--- CodingTracker/CodingSession.cs
+++ CodingTracker/CodingSession.cs
@@ -1,4 +1,9 @@
 public class CodingSession
 {
+    public int Id { get; set; } = 0;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public TimeSpan FullTimeDuration { get; set; }
+
 
 }
