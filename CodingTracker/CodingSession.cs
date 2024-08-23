ff54c6bbb9870046c1eea0795c68905f29405fdb
Jesus Castro <castrojesus39@gmail.com>
2024-08-23 16:31:58 -0700

    updated CodingSession class by adding date

--- CodingTracker/CodingSession.cs
+++ CodingTracker/CodingSession.cs
@@ -1,9 +1,16 @@
+namespace CodingTracker;
+
 public class CodingSession
 {
     public int Id { get; set; } = 0;
+    public DateTime date { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public TimeSpan FullTimeDuration { get; set; }
 
+    public TimeSpan CalculateDuration()
+    {
+        return EndTime - StartTime;
+    }
 
 }
