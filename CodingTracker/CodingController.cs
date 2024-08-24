9bc0bf2c36c8425444253582f5689a095596b247
Jesus Castro <castrojesus39@gmail.com>
2024-08-23 17:03:38 -0700

    added GetDate method

--- CodingTracker/CodingController.cs
+++ CodingTracker/CodingController.cs
@@ -1,8 +1,48 @@
 // Handle all CRUD operations with database here
+using System.Globalization;
 using Dapper;
+
 namespace CodingTracker;
 
-internal class CodingController
+public class CodingController
 {
+    // Get the date
+    public static DateTime getDate() {
+      Console.WriteLine("Please enter the date with the format (dd-mm-yyyy): ");
+      string getDate = Console.ReadLine() ?? "";
 
+      string format = "dd-mm-yyyy";
+
+      CultureInfo provider = CultureInfo.InvariantCulture;
+      bool checkDate = DateTime.TryParseExact(getDate,
+                                              format,
+                                              provider,
+                                              DateTimeStyles.None,
+                                              out DateTime parsedDate);
+      return parsedDate;
+    }
+
+    // Create a session
+    public static void InsertSession()
+    {
+        Console.WriteLine("Insert a new session here!");
+    }
+
+    // Display all times
+    public static void DisplayAllSessions()
+    {
+        Console.WriteLine("Display all sessions");
+    }
+
+    // Update a session
+    public static void UpdateSession()
+    {
+        Console.WriteLine("Update a session");
+    }
+
+    // Delete a session
+    public static void DeleteSession()
+    {
+        Console.WriteLine("Delete a session");
+    }
 }
