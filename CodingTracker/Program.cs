17f35c6b7986e134d21269a899711d409ea59eb8
Jesus Castro <castrojesus39@gmail.com>
2024-08-28 21:55:01 -0700

    refactored main method so that db connectionString is in Menu.cs

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,17 +1,9 @@
-﻿using System.Configuration;
+﻿namespace CodingTracker;
 
-namespace CodingTracker;
-
-internal static class Program
+public class Program
 {
-    private static readonly string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
     public static void Main(string[] args)
     {
-        DatabaseManager databaseManager = new();
-
-        databaseManager.CreateTable(connectionString);
-
         Menu.ShowMenu();
-
     }
 }
