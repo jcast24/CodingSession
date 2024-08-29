51fef04668b899703e7b01cb4099df4f78aca3fc
Jesus Castro <castrojesus39@gmail.com>
2024-08-28 21:55:19 -0700

    added connectionString from dbService

--- CodingTracker/Menu.cs
+++ CodingTracker/Menu.cs
@@ -1,46 +1,72 @@
+using System.Configuration;
 using Spectre.Console;
 
 namespace CodingTracker;
+
 public class Menu()
 {
+    private static readonly string connectionString =
+        ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
+
     public static void ShowMenu()
     {
+        DatabaseService db = new DatabaseService(connectionString);
+
+        
+        Console.Clear();
         // SpectreConsole Test
-        string option = AnsiConsole.Prompt(new SelectionPrompt<string>()
-        .Title("Welcome to Coding Tracker")
-        .AddChoices(new[] {
-            "0. Close the Application", "1. Show all Sessions", "2. Start a new session", "3. Modify a Session", "4. Start", "5. End",
-            })
+        string option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Welcome to Coding Tracker")
+                .AddChoices(
+                    new[]
+                    {
+                        "0. Close the Application",
+                        "1. Show all Sessions",
+                        "2. Start a new session",
+                        "3. Modify a Session",
+                        "4. Delete a session",
+                        "5. Start Timer",
+                        "6. End Timer",
+                    }
+                )
         );
 
         switch (option)
         {
             case "0. Close The Application":
+                Console.WriteLine("Goodbye!");
                 break;
             case "1. Show all Sessions":
                 // Show all sessions
+                CodingController.DisplayAllSessions();
                 break;
             case "2. Start a new session":
                 // Start a new session
+                CodingController.InsertSession();
                 break;
             case "3. Modify a session":
                 // Modify a session
-                // Allows the user to enter a new time
+                // Allows the user to enter a new time, new date
+                CodingController.UpdateSession();
                 break;
-            case "4. Start":
-                // Start timer 
-                // If the timer has already started then, shoot message "Timer has been running"
+            case "4. Delete a session":
+                CodingController.DeleteSession();
                 break;
-            case "5. End":
-                // End timer 
+            case "5. Start Timer":
+                // Start timer
+                // If the timer has already started, then shoot message "Timer has been running"
+                Console.WriteLine("Timer is running");
+                Watch.Start();
+                break;
+            case "6. End Timer":
+                // End timer
                 // If the time isn't running, shoot message "Timer never started"
+                Console.WriteLine("Timer has ended");
                 break;
             default:
                 Console.WriteLine("Please choose an option!");
                 break;
-
-
         }
-
     }
 }
