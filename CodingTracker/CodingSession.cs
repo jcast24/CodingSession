788093ef532891e01ea4c44f59f4dfded15ba995
Jesus Castro <castrojesus39@gmail.com>
2024-09-13 16:29:32 -0700

    updated version of CodingSession, should still be the same

--- CodingTracker/CodingSession.cs
+++ CodingTracker/CodingSession.cs
@@ -4,13 +4,12 @@ public class CodingSession
 {
     public int Id { get; set; } = 0;
     public DateTime date { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
     public TimeSpan FullTimeDuration { get; set; }
 
     public TimeSpan CalculateDuration()
     {
         return EndTime - StartTime;
     }
-
 }
