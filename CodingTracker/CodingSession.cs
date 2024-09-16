51a9bbbcf962f41cb511b9ebc1d8964d90f89d5b
Jesus Castro <castrojesus39@gmail.com>
2024-09-15 19:41:50 -0700

    removed CalcDuration method from class, not sure how to implement yet

--- CodingTracker/CodingSession.cs
+++ CodingTracker/CodingSession.cs
@@ -3,13 +3,8 @@ namespace CodingTracker;
 public class CodingSession
 {
     public int Id { get; set; } = 0;
-    public DateTime date { get; set; }
+    public string? date { get; set; }
     public TimeSpan StartTime { get; set; }
     public TimeSpan EndTime { get; set; }
     public TimeSpan FullTimeDuration { get; set; }
-
-    public TimeSpan CalculateDuration()
-    {
-        return EndTime - StartTime;
-    }
 }
