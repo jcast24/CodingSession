ac4fcbec0eba3a67116bd97a056707fdaaf8229e
Jesus Castro <castrojesus39@gmail.com>
2024-10-02 22:16:43 -0700

    added functionality where menu comes back up when the user chooses an option

--- CodingTracker/Menu.cs
+++ CodingTracker/Menu.cs
@@ -17,59 +17,65 @@ public class Menu()
 
         engine.CreateTable();
 
-        // SpectreConsole Test
-        string option = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("Welcome to Coding Tracker")
-                .AddChoices(
-                    new[]
-                    {
-                        "0. Close the Application",
-                        "1. Show all Sessions",
-                        "2. Start a new session",
-                        "3. Modify a Session",
-                        "4. Delete a session",
-                        "5. Start Timer",
-                        "6. End Timer",
-                    }
-                )
-        );
+        bool keepRunning = true;
 
-        switch (option)
+        while (keepRunning)
         {
-            case "0. Close The Application":
-                Console.WriteLine("Goodbye!");
-                break;
-            case "1. Show all Sessions":
-                // Show all sessions
-                engine.DisplayAllSessions();
-                break;
-            case "2. Start a new session":
-                // Start a new session
-                engine.InsertSession();
-                break;
-            case "3. Modify a Session":
-                // Modify a session
-                // Allows the user to enter a new time, new date
-                engine.UpdateSession();
-                break;
-            case "4. Delete a session":
-                engine.DeleteSession();
-                break;
-            case "5. Start Timer":
-                // Start timer
-                // If the timer has already started, then shoot message "Timer has been running"
-                Console.WriteLine("Timer is running");
-                Watch.Start();
-                break;
-            case "6. End Timer":
-                // End timer
-                // If the time isn't running, shoot message "Timer never started"
-                Console.WriteLine("Timer has ended");
-                break;
-            default:
-                Console.WriteLine("Please choose an option!");
-                break;
+            string option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Welcome to Coding Tracker")
+                    .AddChoices(
+                        new[]
+                        {
+                            "0. Close the Application",
+                            "1. Show all Sessions",
+                            "2. Start a new session",
+                            "3. Modify a Session",
+                            "4. Delete a session",
+                            "5. Start Timer",
+                            "6. End Timer",
+                        }
+                    )
+            );
+
+            switch (option)
+            {
+                case "0. Close the Application":
+                    keepRunning = false;
+                    Console.WriteLine("Goodbye!");
+                    break;
+                case "1. Show all Sessions":
+                    // Show all sessions
+                    engine.DisplayAllSessions();
+                    break;
+                case "2. Start a new session":
+                    // Start a new session
+                    engine.InsertSession();
+                    break;
+                case "3. Modify a Session":
+                    // Modify a session
+                    // Allows the user to enter a new time, new date
+                    engine.UpdateSession();
+                    break;
+                case "4. Delete a session":
+                    engine.DeleteSession();
+                    break;
+                case "5. Start Timer":
+                    // Start timer
+                    // If the timer has already started, then shoot message "Timer has been running"
+                    Console.WriteLine("Timer is running");
+                    Watch.Start();
+                    break;
+                case "6. End Timer":
+                    // End timer
+                    // If the time isn't running, shoot message "Timer never started"
+                    Console.WriteLine("Timer has ended");
+                    break;
+                default:
+                    Console.WriteLine("Please choose an option!");
+                    break;
+            }
         }
+        // SpectreConsole Test
     }
 }
